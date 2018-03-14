437bdf42db3a0b477753561fa552486360bc7c55
parth7676 <parth7676@gmail.com>
2018-03-14 13:29:20 -0400

    Added Sample template

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=g1maestro-Web-Deploy.pubxml -p:Password=cnlfHTzu9PJ9k0qPpwTAMFkAcTyh63bfxmh7wLuco6tNbzzlnkMbSiLivFtL
+
